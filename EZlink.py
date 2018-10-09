3885c4b5032f1a261d74ff0fba64da3c0f5cf536
ckhan <chungkyun.han@gmail.com>
2018-10-09 13:33:49 +0800

    t

--- EZlink.py
+++ EZlink.py
@@ -1,22 +1,39 @@
 import os.path as opath
 import os
-from functools import reduce
-
 import csv
-
+from functools import reduce
+#
+from __path_organizer import pf_dpath
 
 EZ_RAW_DATA_HOME = reduce(opath.join, [opath.expanduser("~"), '..', 'SMART', 'ezlink_2013_08'])
 
-for fn in os.listdir(EZ_RAW_DATA_HOME):
-    if not fn.endswith('.csv'):
-        continue
-    fpath = opath.join(EZ_RAW_DATA_HOME, fn)
 
-    with open(fpath) as r_csvfile:
-        reader = csv.DictReader(r_csvfile)
-        for row in reader:
-            print([row[cn] for cn in ['JOURNEY_ID', 'CARD_ID', 'PASSENGERTYPE', 'TRAVEL_MODE',
-                                'BOARDING_STOP_STN', 'ALIGHTING_STOP_STN',
-                                'RIDE_START_DATE', 'RIDE_START_TIME', 'RIDE_DISTANCE', 'RIDE_TIME']])
-            assert False
+def run():
+    dpath = opath.join(pf_dpath, 'EZlink')
+    if not opath.exists(dpath):
+        os.mkdir(dpath)
+    #
+    header = ['JOURNEY_ID', 'CARD_ID', 'PASSENGERTYPE', 'TRAVEL_MODE',
+                'BOARDING_STOP_STN', 'ALIGHTING_STOP_STN',
+                'RIDE_START_DATE', 'RIDE_START_TIME', 'RIDE_DISTANCE', 'RIDE_TIME']
+    for fn in os.listdir(EZ_RAW_DATA_HOME):
+        if not fn.endswith('.csv'):
+            continue
+        ifpath = opath.join(EZ_RAW_DATA_HOME, fn)
+        with open(ifpath) as r_csvfile:
+            reader = csv.DictReader(r_csvfile)
+            for row in reader:
+                cid = row['CARD_ID']
+                ofpath = opath.join(dpath, 'EZlink-%s.csv' % cid)
+                if not opath.exists(ofpath):
+                    with open(ofpath, 'w') as w_csvfile:
+                        writer = csv.writer(w_csvfile, lineterminator='\n')
+                        writer.writerow(header)
+                with open(ofpath, 'a') as w_csvfile:
+                    writer = csv.writer(w_csvfile, lineterminator='\n')
+                    writer.writerow([row[cn] for cn in header])
 
+
+
+if __name__ == '__main__':
+    run()
