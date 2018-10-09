437616655be7d51b5c14e3ac0fe96cb7a4a05216
ckhan <chungkyun.han@gmail.com>
2018-10-09 14:03:52 +0800

    t

--- EZlink.py
+++ EZlink.py
@@ -1,14 +1,17 @@
 import os.path as opath
 import os
 import csv
+import pandas as pd
+from datetime import datetime
 from functools import reduce
+import multiprocessing
 #
 from __path_organizer import pf_dpath
 
 EZ_RAW_DATA_HOME = reduce(opath.join, [opath.expanduser("~"), '..', 'SMART', 'ezlink_2013_08'])
 
 
-def run():
+def process_raw_data():
     dpath = opath.join(pf_dpath, 'EZlink')
     if not opath.exists(dpath):
         os.mkdir(dpath)
@@ -34,6 +37,37 @@ def run():
                     writer.writerow([row[cn] for cn in header])
 
 
+def sort_individual_transaction():
+    dpath = opath.join(pf_dpath, 'EZlink')
+    #
+    def process_files(_, fns):
+        for fn in fns:
+            ifpath = opath.join(dpath, fn)
+            df = pd.read_csv(ifpath)
+            timestamp = []
+            for _, row in df.iterrows():
+                year, month, day = [int(v) for v in row['RIDE_START_DATE'].split('-')]
+                hour, minute, second = [int(v) for v in row['RIDE_START_TIME'].split(':')]
+                dt = datetime(year, month, day, hour, minute, second)
+                timestamp.append(dt.timestamp())
+            df['TIMESTAMP'] = pd.Series(timestamp, index=df.index)
+            df = df.sort_values(by=['TIMESTAMP'])
+            df.to_csv(ifpath, index=False)
+    #
+    numProcessors = multiprocessing.cpu_count()
+    worker_fns = [[] for _ in range(numProcessors)]
+    for i, fn in enumerate(sorted([fn for fn in os.listdir(dpath) if fn.endswith('.csv')])):
+        worker_fns[i % numProcessors].append(fn)
+    ps = []
+    for wid, fns in enumerate(worker_fns):
+        p = multiprocessing.Process(target=process_files,
+                                    args=(wid, fns))
+        ps.append(p)
+        p.start()
+    for p in ps:
+        p.join()
+
 
 if __name__ == '__main__':
-    run()
+    # process_raw_data()
+    sort_individual_transaction()
