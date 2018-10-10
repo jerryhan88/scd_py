f0da75d419aab0d86bb5336b73c17dde703103fc
ckhan <chungkyun.han@gmail.com>
2018-10-10 10:18:41 +0800

    t

--- EZlink.py
+++ EZlink.py
@@ -9,6 +9,7 @@ import multiprocessing
 from __path_organizer import pf_dpath
 
 EZ_RAW_DATA_HOME = reduce(opath.join, [opath.expanduser("~"), '..', 'SMART', 'ezlink_2013_08'])
+NUM_GROUP = 20
 
 
 def process_raw_data():
@@ -37,6 +38,32 @@ def process_raw_data():
                     writer.writerow([row[cn] for cn in header])
 
 
+def process_raw_data1():
+    dpath = opath.join(pf_dpath, 'EZlink1')
+    if not opath.exists(dpath):
+        os.mkdir(dpath)
+    #
+    header = ['JOURNEY_ID', 'CARD_ID', 'PASSENGERTYPE', 'TRAVEL_MODE',
+                'BOARDING_STOP_STN', 'ALIGHTING_STOP_STN',
+                'RIDE_START_DATE', 'RIDE_START_TIME', 'RIDE_DISTANCE', 'RIDE_TIME']
+    for fn in os.listdir(EZ_RAW_DATA_HOME):
+        if not fn.endswith('.csv'):
+            continue
+        ifpath = opath.join(EZ_RAW_DATA_HOME, fn)
+        with open(ifpath) as r_csvfile:
+            reader = csv.DictReader(r_csvfile)
+            for row in reader:
+                cid = int(row['CARD_ID'])
+                ofpath = opath.join(dpath, 'EZlink-g%d.csv' % (cid % NUM_GROUP))
+                if not opath.exists(ofpath):
+                    with open(ofpath, 'w') as w_csvfile:
+                        writer = csv.writer(w_csvfile, lineterminator='\n')
+                        writer.writerow(header)
+                with open(ofpath, 'a') as w_csvfile:
+                    writer = csv.writer(w_csvfile, lineterminator='\n')
+                    writer.writerow([row[cn] for cn in header])
+
+
 def sort_individual_transaction():
     dpath = opath.join(pf_dpath, 'EZlink')
     #
@@ -51,7 +78,7 @@ def sort_individual_transaction():
                 dt = datetime(year, month, day, hour, minute, second)
                 timestamp.append(dt.timestamp())
             df['TIMESTAMP'] = pd.Series(timestamp, index=df.index)
-            df = df.sort_values(by=['TIMESTAMP'])
+            df = df.sort_values(by=['CARD_ID', 'TIMESTAMP'])
             df.to_csv(ifpath, index=False)
     #
     numProcessors = multiprocessing.cpu_count()
@@ -70,4 +97,5 @@ def sort_individual_transaction():
 
 if __name__ == '__main__':
     # process_raw_data()
-    sort_individual_transaction()
+    process_raw_data1()
+    # sort_individual_transaction()
