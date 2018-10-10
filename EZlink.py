8d8c1e24071f09fe3b7dffcf30b1fef2ff3f22d8
ckhan <chungkyun.han@gmail.com>
2018-10-10 15:39:47 +0800

    t

--- EZlink.py
+++ EZlink.py
@@ -71,18 +71,33 @@ def process_raw_data():
 
 def sort_transactions():
     dpath = opath.join(pf_dpath, 'EZlink1')
-    for fn in sorted([fn for fn in os.listdir(dpath) if fn.endswith('.csv')]):
-        ifpath = opath.join(dpath, fn)
-        df = pd.read_csv(ifpath)
-        timestamp = []
-        for _, row in df.iterrows():
-            year, month, day = [int(v) for v in row['RIDE_START_DATE'].split('-')]
-            hour, minute, second = [int(v) for v in row['RIDE_START_TIME'].split(':')]
-            dt = datetime(year, month, day, hour, minute, second)
-            timestamp.append(dt.timestamp())
-        df['TIMESTAMP'] = pd.Series(timestamp, index=df.index)
-        df = df.sort_values(by=['CARD_ID', 'TIMESTAMP'])
-        df.to_csv(ifpath, index=False)
+    #
+    def process_files(_, fns):
+        for fn in fns:
+            ifpath = opath.join(dpath, fn)
+            df = pd.read_csv(ifpath)
+            timestamp = []
+            for _, row in df.iterrows():
+                year, month, day = [int(v) for v in row['RIDE_START_DATE'].split('-')]
+                hour, minute, second = [int(v) for v in row['RIDE_START_TIME'].split(':')]
+                dt = datetime(year, month, day, hour, minute, second)
+                timestamp.append(dt.timestamp())
+            df['TIMESTAMP'] = pd.Series(timestamp, index=df.index)
+            df = df.sort_values(by=['CARD_ID', 'TIMESTAMP'])
+            df.to_csv(ifpath, index=False)
+
+    numProcessors = multiprocessing.cpu_count()
+    worker_fns = [[] for _ in range(numProcessors)]
+    for i, fn in enumerate(sorted([fn for fn in os.listdir(dpath) if fn.endswith('.csv')])):
+        worker_fns[i % numProcessors].append(fn)
+    ps = []
+    for wid, fns in enumerate(worker_fns):
+        p = multiprocessing.Process(target=process_files,
+                                    args=(wid, fns))
+        ps.append(p)
+        p.start()
+    for p in ps:
+        p.join()
 
 
 if __name__ == '__main__':
